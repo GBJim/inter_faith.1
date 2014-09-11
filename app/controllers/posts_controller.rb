2448d47f2d9849238f4ccb98a69e9a36785d2c0a
GB <gb@ubuntu.ubuntu-domain>
2014-09-11 19:49:47 +0800

    Add embedly media in show, but no design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -167,6 +167,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :image, :user_id, :tag_list, religion_ids: [] )
+      params.require(:post).permit(:title, :body, :url, :user_id, :tag_list, religion_ids: [] )
     end
 end
